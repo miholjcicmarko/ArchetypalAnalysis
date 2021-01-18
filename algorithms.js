98d96768016218ae573c1899b34763e2ec628b06
miholjcicmarko <u0984549@utah.edu>
2021-01-17 22:21:16 -0700

    update

---
+++ algorithms.js
@@ -0,0 +1,26 @@
+/** Class implementing the Algorithms on the data. */
+class Algorithms {
+
+    constructor(data, noc, ini_obs) {
+        this.data = data;
+        this.noc = noc;
+        this.ini_obs = ini_obs; 
+    }
+
+    furthest_Sum() {
+
+        let dimensions = [this.data.length, this.data[0].length];
+        let index = [];
+
+        for (let i = 0; i < this.data[0].length; i++) {
+            index.push(i);
+        }
+
+        
+        
+
+
+    }
+
+
+}
