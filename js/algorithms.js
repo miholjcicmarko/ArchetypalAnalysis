46b28d28787cd1b22010161f629e3d992da9a3aa
miholjcicmarko <u0984549@utah.edu>
2021-01-18 18:18:07 -0700

    update

--- js/algorithms.js
+++ js/algorithms.js
@@ -12,16 +12,16 @@ class Algorithms {
 
         this.ini_obs = ini_obs; 
 
-        let dimensions = [this.data[0].length, this.data.length];
+        let dimensions = [this.data[0].length, this.data[0].columns.length];
         let index = [];
 
-        for (let i = 0; i < this.data.length; i++) {
+        for (let i = 0; i < this.data[0].columns.length; i++) {
             index.push(i);
         }
 
-        index[this.ini_obs+1] = 0;
+        index[this.ini_obs] = -1;
 
-        ind_t = this.ini_obs;
+        let ind_t = this.ini_obs;
 
         let sum_dist = [];
 
@@ -31,14 +31,18 @@ class Algorithms {
 
         if (this.data.length > this.noc * this.data.length) {
             kt = this.data;
-            kt_2 = kt.array.forEach(element, kt_index, kt_array => {
+            kt_temp = kt.array.forEach(element, kt_index, kt_array => {
                 kt_array[kt_index] = element * element;
             });
+            kt_2 = math.sum(kt_temp);
             
             for (let i = 1; i < this.noc + 10; i++) {
                 if (i > this.noc - 1) {
                     kq = math.dot(kt[0][ini_obs[0]],kt);
-
+                    sum_dist[i] -=  math.sqrt(kt_2 - 2*kq + kt_2[ind_t]);
+                    index[ini_obs[0]] = ini_obs[0];
+                    ini_obs = [];
+                    t = index.find(element => element != -1);
                 }
             }
 
