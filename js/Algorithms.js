c8005d5ef25e634519dcedb0e5f3f7c75f9687e5
miholjcicmarko <u0984549@utah.edu>
2021-01-21 01:53:43 -0700

    update

--- js/Algorithms.js
+++ js/Algorithms.js
@@ -1,5 +1,7 @@
 /** Class implementing the Algorithms on the data. */
 
+const { matrix, number, null } = require("mathjs");
+
 // import { create, all } from 'mathjs';
 
 // const config = { };
@@ -18,6 +20,164 @@ class Algorithms {
         
         // let num = mathjs.square(4);
         // console.log(num);
+    }
+
+    phca(X, noc, I, U, delta, verbose, conv_crit, maxiter) {
+
+        let conv_crit = 1*e^-6;
+        let maxiter = 500;
+
+        [N, M] = math.size(X);
+
+        if (I === null) {
+            I = math.range(M);
+        }
+        if (U === null) {
+            U = math.range(M);
+        }
+
+        // 149
+
+
+    }
+
+    S_update(S, XCtX, CtXtXC, muS, SST, SSE, niter) {
+
+        let [noc, J] = math.size(S);
+
+        let e = math.ones(noc, 1);
+
+        for (let k = 0; k < niter; k++) {
+            let SSE_old = SSE;
+            let g_temp1 = math.subtract((math.dot(CtXtXC, S), XCtX))
+            let g_temp2 = math.divide(SST, J);
+            let g = math.divide(g_temp1,g_temp2);
+
+            g = math.subtract(g, math.multiply(e, math.sum(math.multiply(g,S))));
+
+            S_old = S
+            while (true) {
+                let S = math.subtract(S_old, math.multiply(g,muS));
+
+                for (let p = 0; p < S.length; p++) {
+                    for (let n = 0; n < S[p].length; n++) {
+                        if (S[p][n] < 0) {
+                            S[p][n] = 0;
+                        }
+                    }
+                }
+
+                S = math.divide(S, math.dot(e, math.sum(S)));
+                let SSt = math.multiply(S, math.transpose(S));
+
+                let SSE = math.subtract(SST, math.multiply(2, math.sum(math.multiply(XCtX,S)) + 
+                            math.sum(math.multiply(CtXtXC,SSt))));
+
+                if (SSE <= SSE_old * (1 + 1e-9)) {
+                    muS = math.multiply(muS, 1.2);
+                    break;
+                }
+                else {
+                    muS = math.divide(muS, 2);
+                }
+            }
+        }
+        return S, SSE, muS, SSt;
+    }
+
+    C_update(X, XSt, XC, SSt, C, delta, muC, mualpha, SST, SSE, niter) {
+        let [noc, J] = math.size(C);
+
+        if (delta != 0) {
+            alphaC = math.sum(C);
+            C = math.dot(C, math.diag(math.divide(1,alphaC)));
+        }
+
+        let e = math.ones(J, 1);
+
+        let XtXSt = math.dot(math.transpose(X), XSt);
+
+        for (let k = 0; k < niter; k++) {
+            let SSE_old = SSE;
+            let g_temp1 = math.subtract((math.dot(math.transpose(X), math.dot(XC,SSt)),XtXSt)); 
+            let g = math.divide(g_temp1,SST);
+
+            if (delta != 0) {
+                g = math.dot(g, math.diag(alphaC))
+            }
+            g = math.subtract(g, math.multiply(e,math.sum(math.multiply(g,C))));
+
+            C_old = C;
+            while (true) {
+                let C = math.subtract(C_old, math.multiply(muC,g));
+
+                for (let p = 0; p < C.length; p++) {
+                    for (let n = 0; n < C[p].length; n++) {
+                        if (C[p][n] < 0) {
+                            C[p][n] = 0;
+                        }
+                    }
+                }
+
+                nC = math.sum(C) + Number.EPSILON;
+                C = math.dot(C, math.diag(math.divide(1, nC[0])));
+
+                if (delta != 0) {
+                    Ct = math.multiply(C,math.diag(alphaC));
+                }
+                else {
+                    Ct = C;
+                }
+
+                let XC = math.dot(X, Ct);
+                let CtXtXC = math.dot(math.transpose(XC), XC);
+                let SSE_temp = math.subtract(SST,math.multiply(2,math.sum(XC,XSt)));
+                let SSE = math.add(SSE_temp, math.sum(math.multiply(CtXtXC,SSt)));
+
+                if (SSE <= SSE_old * (1 + 1e-9)) {
+                    muC = math.multiply(muC,1.2);
+                    break;
+                }
+                else {
+                    muC = math.divide(muC,2);
+                }
+            }
+
+            SSE_old = SSE;
+            if (delta != 0) {
+                let g_temp1 = math.transpose(math.diag(math.multiply(CtXtXC, SSt)));  
+                let g_temp2 = math.divide(g_temp1,alphaC);
+                let g_temp3 = math.sum(math.multiply(C,XtXSt));
+                let g_temp4 = math.divide(g_temp3, math.multiply(SST,J));
+                let g = math.subtract(g_temp2, g_temp4);
+
+                alphaC_old = alphaC;
+
+                while (true) {
+                    alphaC = math.subtract(alphaC_old, math.multiply(mualpha,g));
+                    alphaC[alphaC < 1 - delta] = 1 - delta;
+                    alphaC[alphaC > 1 + delta] = 1 + delta;
+
+                    let XCt = math.dot(XC, math.diag(math.divide(alphaC,alphaC_old)));
+                    CtXtXC = math.dot(math.transpose(XCt), XCt);
+                    let SSE_temp = math.subtract(SST,math.multiply(2,math.sum(XCt,XSt)));
+                    let SSE = math.add(SSE_temp, math.sum(math.multiply(CtXtXC,SSt)));
+
+                    if (SSE <= SSE_old * (1 + 1e-9)) {
+                        mualpha = math.multiply(mualpha,1.2);
+                        XC = XCt;
+                        break;
+                    }
+                    else {
+                        mualpha = mualpha.divide(mualpha,2);
+                    }   
+                }
+            }
+        }
+        if (delta != 0) {
+            C = math.multiply(C,math.diag(alphaC));
+        }
+        return C, SSE, muC, mualpha, CtXtXC, XC;
     }
 
     furthest_Sum(ini_obs) {
@@ -58,8 +218,6 @@ class Algorithms {
             }
             let kt_temp = math.square(kt);
             
-
-
             // for (let l = 0; l < this.data[0].length; l++) {
             //     for (let p = 0; p < this.data[0].columns.length; p++) {
             //         let squared_elem = this.data[0][l][p] * this.data[0][l][p];
