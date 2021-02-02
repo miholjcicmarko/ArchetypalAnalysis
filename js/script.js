06bef6a0da4e804c54c27d938f5d0e1a73eb33f1
miholjcicmarko <u0984549@utah.edu>
2021-02-01 21:08:23 -0700

    update

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ let data = d3.csv("./data/COVID19.csv");
 Promise.all([data]).then(data => {
 
     let aa_result = new Algorithms(data, 3);
+ 
+    let plots = new visuals(aa_result,data);
+    plots.addBars();
 
-    let plots = new visuals(aa_result);
 })
