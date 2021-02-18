e7ffe3900a6cc2a4bf903261341d69199b926c92
Marko Miholjcic <miholjcicmarko@yahoo.com>
2021-02-17 23:59:26 -0700

    update

--- js/script.js
+++ js/script.js
@@ -14,12 +14,14 @@ async function loadSandXC() {
     }
 }
 
-let data = d3.csv("./data/COVID19.csv");
-//let data = loadSandXC();
+//let data = d3.csv("./data/COVID19.csv");
+let data = loadSandXC();
 
 Promise.all([data]).then(data => {
 
-    let aa_result = new Algorithms(data, 3);
+    let plots = new aa_view(data);
+
+    //let aa_result = new Algorithms(data, 3);
  
     //let plots = new visuals(data);
     
