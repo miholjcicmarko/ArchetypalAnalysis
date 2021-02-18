34d75c31fe3852adaf1337e43f286d088d843a94
Marko Miholjcic <miholjcicmarko@yahoo.com>
2021-02-18 00:07:01 -0700

    update

---
+++ js/aa_view.js
@@ -0,0 +1,21 @@
+class aa_view {
+    
+    constructor(data) {
+
+        this.XC = data[0].XC;
+        this.S = data[0].S;
+    
+        this.raw = data[0].raw; 
+    
+        this.timeline = data[0].time_data;
+    
+        // this.XC = phca_result.XC;
+        // this.S = phca_result.S;
+    
+        // this.data = data;
+    
+        // this.matrix_data = math.matrix(this.XC);
+    
+    }
+}
+
