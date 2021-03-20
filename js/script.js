57d18a2193d82de62fd91446f95637ae0d842093
Marko Miholjcic <miholjcicmarko@yahoo.com>
2021-03-20 02:25:37 -0600

    update

--- js/script.js
+++ js/script.js
@@ -77,19 +77,18 @@ Promise.all([preProcessData]).then(data => {
         }
         else {
             let aa_result = new Algorithms(data, numArch);
-            let matricies = result_to_Object(aa_result, data);
+            let matricies = result_to_Object(aa_result);
             let plots = new aa_view(matricies, numArch, updateArch);
         }
     }
 
-    function result_to_Object(result, data) {
+    function result_to_Object(result) {
         return {
             "XC": result.XC,
             "S": result.S,
             "raw": result.origData,
             "data_id_less": result.data,
             "timeData": result.timeSeries
-
         }
     }
 
